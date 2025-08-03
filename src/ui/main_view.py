1b9dd9a2137d384277687acc3bd100bf8e97d1d9
jrdimande <timoteojrdimande@gmail.com>
2025-08-03 03:53:48 +0200

    feat(main_view): add function send_message

--- src/ui/main_view.py
+++ src/ui/main_view.py
@@ -5,21 +5,21 @@ class MainView:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Coherebot")
-        self.root.config(bg="#2f3030")
+        self.root.config(bg="white")
         self.root.geometry("950x700")
         self.root.resizable(False, False)
         self.cohere_client = CohereClient()
 
-        sidebar = tk.Frame(self.root, bg="#0c0c0d", width=200)
+        sidebar = tk.Frame(self.root, bg="#b4a1ff", width=200)
         sidebar.pack(side="left", fill="y")
 
 
-        self.responses_text = tk.Text(self.root, width=52, height=20, font=("Courier New", 12), bg="#2f3030", fg="white", bd=0)
+        self.responses_text = tk.Text(self.root, width=52, height=20, font=("Courier New", 12), bg="white", fg="black", bd=0)
         self.responses_text.place(x=280, y=90)
         self.responses_text.insert(tk.END, "Hello World")
         #responses_text.config(state="disabled")
 
-        self.message_text = tk.Text(self.root, width=75, height=5, pady=5, bd=1, bg="#434445", fg="white", font=("Arial", 12))
+        self.message_text = tk.Text(self.root, width=75, height=5, pady=5, bd=1, bg="#f3f2f7", fg="black", font=("Arial", 12))
         self.message_text.place(x=233, y=550)
 
         self.message_text.bind("<Return>", self.send_message)
