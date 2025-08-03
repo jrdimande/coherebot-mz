149981bf18a138637f78bb97e43ea8627eefb7c3
jrdimande <timoteojrdimande@gmail.com>
2025-08-03 04:22:26 +0200

    feat(main_view): add function send_message

--- src/ui/main_view.py
+++ src/ui/main_view.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from src.core.cohere_service import CohereClient
+from tkinter import PhotoImage
 
 class MainView:
     def __init__(self):
@@ -10,14 +11,24 @@ class MainView:
         self.root.resizable(False, False)
         self.cohere_client = CohereClient()
 
+
+        # Sidebar
         sidebar = tk.Frame(self.root, bg="#b4a1ff", width=200)
         sidebar.pack(side="left", fill="y")
+
+        # Image logo
+        self.logo_img = PhotoImage(file="../assets/cohere_logo.png")
+        self.logo_btn = tk.Button(self.root, image=self.logo_img, bg="white", bd=0)
+        self.logo_btn.place(x=15, y=15)
+
+        # Text logo
+        self.text_label_logo = tk.Label(self.root, text="CohereBot", bg="#b4a1ff", fg="#184a22" ,font=("Arial", 16, "bold"))
+        self.text_label_logo.place(x=60, y=18)
 
 
         self.responses_text = tk.Text(self.root, width=52, height=20, font=("Courier New", 12), bg="white", fg="black", bd=0)
         self.responses_text.place(x=280, y=90)
-        self.responses_text.insert(tk.END, "Hello World")
-        #responses_text.config(state="disabled")
+        self.responses_text.insert(tk.END, "Hi, how can i assist you today?")
 
         self.message_text = tk.Text(self.root, width=75, height=5, pady=5, bd=1, bg="#f3f2f7", fg="black", font=("Arial", 12))
         self.message_text.place(x=233, y=550)
